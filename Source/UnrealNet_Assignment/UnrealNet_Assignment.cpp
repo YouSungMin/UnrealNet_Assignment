324f65e7b6a51a8e7b471bdb7ddbb741ae1c464b
YouSungMin <xormraoghk@gmail.com>
2026-01-16 15:33:41 +0900

    Initial commit

---
+++ Source/UnrealNet_Assignment/UnrealNet_Assignment.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UnrealNet_Assignment.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealNet_Assignment, "UnrealNet_Assignment" );
