ad7376890b9be64c72b04d35edba46188142aba5
YouSungMin <xormraoghk@gmail.com>
2026-01-19 17:57:33 +0900

    0119_스포너 완성

---
+++ Source/UnrealNet_Assignment/Private/Items/PickupSpawner.cpp
@@ -0,0 +1,71 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Items/PickupSpawner.h"
+#include "NavigationSystem.h"
+#include "Items/ScorePickup.h"
+#include "Components/SphereComponent.h"
+
+// Sets default values
+APickupSpawner::APickupSpawner()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	SpawnRadiusComp = CreateDefaultSubobject<USphereComponent>(TEXT("SpawnRadiusComp"));
+	RootComponent = SpawnRadiusComp;
+
+	SpawnRadiusComp->SetCollisionProfileName(TEXT("NoCollision"));
+}
+
+// Called when the game starts or when spawned
+void APickupSpawner::BeginPlay()
+{
+	Super::BeginPlay();
+
+	NavSystem = UNavigationSystemV1::GetCurrent(GetWorld());
+
+	if (HasAuthority())
+	{
+		// 3초마다 SpawnItem 함수 반복 호출 (루프: true)
+		GetWorld()->GetTimerManager().SetTimer(
+			SpawnTimerHandle,
+			this,
+			&APickupSpawner::SpawnItem,
+			SpawnInterval,
+			true
+		);
+	}
+}
+
+void APickupSpawner::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	if (SpawnRadiusComp)
+	{
+		// SpawnRadius 변수 값에 맞춰서 구체 크기 변경
+		SpawnRadiusComp->SetSphereRadius(SpawnRadius);
+	}
+}
+
+void APickupSpawner::SpawnItem()
+{
+	if (!ItemClass && !NavSystem.IsValid()) return;
+
+	FNavLocation RandomLocation;
+
+	bool bFound = NavSystem->GetRandomPointInNavigableRadius(
+		GetActorLocation(),
+		SpawnRadius,
+		RandomLocation
+	);
+
+	if (bFound)
+	{
+		FVector SpawnPos = RandomLocation.Location + FVector(0, 0, 50.0f);
+		FRotator SpawnRot = FRotator::ZeroRotator;
+
+		GetWorld()->SpawnActor<AActor>(ItemClass, SpawnPos, SpawnRot);
+	}
+}
+
---
+++ Source/UnrealNet_Assignment/Public/Items/PickupSpawner.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PickupSpawner.generated.h"
+
+UCLASS()
+class UNREALNET_ASSIGNMENT_API APickupSpawner : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APickupSpawner();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+	void SpawnItem();
+
+protected:
+	UPROPERTY()
+	TWeakObjectPtr<class UNavigationSystemV1> NavSystem;
+
+	UPROPERTY(EditAnywhere, Category = "Spawner")
+	TSubclassOf<class AActor> ItemClass;
+
+	UPROPERTY(EditAnywhere, Category = "Spawner")
+	float SpawnRadius = 1000.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Spawner")
+	float SpawnInterval = 3.0f;
+
+	UPROPERTY(VisibleAnywhere, Category = "Spawner")
+	TObjectPtr<class USphereComponent> SpawnRadiusComp;
+
+	FTimerHandle SpawnTimerHandle;
+};
